1ffd14338023a0e90aa26d6c5e9fa4e4331a3b41
arashid854 <52444747+arashid854@users.noreply.github.com>
2020-03-09 16:16:29 +0530

    Updates

---
+++ precache-manifest.421594388b99c0eef1e8793e9391bfcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2aa211d7f1829b1b51c77e6bad5bc44",
+    "url": "/face_recognition/index.html"
+  },
+  {
+    "revision": "cb4a028814cdc1676eb9",
+    "url": "/face_recognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "4ece9c689b61d56a371e",
+    "url": "/face_recognition/static/css/main.3528fa2c.chunk.css"
+  },
+  {
+    "revision": "cb4a028814cdc1676eb9",
+    "url": "/face_recognition/static/js/2.fcc1f2fb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/face_recognition/static/js/2.fcc1f2fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ece9c689b61d56a371e",
+    "url": "/face_recognition/static/js/main.c6bb3b57.chunk.js"
+  },
+  {
+    "revision": "87a74e16f8dd685aa8bc",
+    "url": "/face_recognition/static/js/runtime-main.2b9f0b3c.js"
+  }
+]);
